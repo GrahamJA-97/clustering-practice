68c39e1fd5441339cc6e486a85d24f6ab682d071
chrisschulz131 <vbts13@gmail.com>
2019-11-19 17:10:50 -0500

    compressed image2 for first question about RSS. prints inertia every iteration when ran.

--- imgCompKMeans.py
+++ imgCompKMeans.py
@@ -3,11 +3,13 @@
 # purpose: K-Means compression on an image
 
 import numpy as np
+from PIL import Image
 
 from matplotlib.image import imread
 import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances_argmin_min
 from sklearn.preprocessing import StandardScaler
 
 import argparse
@@ -30,9 +32,11 @@ def parseArguments():
 
     return parser.parse_args()
 
+
 def main():
     parms = parseArguments()
-
+    k = parms.k
+    print("\nK value = ", k)
     img = imread(parms.imageFileName)
     img_size = img.shape
 
@@ -44,15 +48,23 @@ def main():
     # Insert your code here to perform
     # -- KMeans clustering
     # -- replace colors in the image with their respective centroid
+    means = KMeans(init='random', n_init=1, max_iter=10, n_clusters=k, verbose='true')
+    means.fit(X)
 
+    # replaces colors with their centroid values.
+    centroids = np.array([list(means.cluster_centers_[label]) for label in means.labels_])
+    centroids = centroids.astype("uint8")
 
     # save modified image (code assumes new image in a variable
     # called X_compressed)
-    # Reshape to have the same dimension as the original image 
+    # Reshape to have the same dimension as the original image
 
-    X_compressed = X_compressed.reshape(img_size[0], img_size[1], img_size[2])
+    X_compressed = np.reshape(centroids, (img_size[0], img_size[1], img_size[2]))
 
     fig, ax = plt.subplots(1, 1, figsize = (8, 8))
+
+    Rss11 = Image.fromarray(X_compressed)
+    Rss11.save("Rss1-1.jpg")
 
     ax.imshow(X_compressed)
     for ax in fig.axes:
