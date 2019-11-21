454e8a52a0d8f31ea2b571c065af238420dcef40
chrisschulz131 <vbts13@gmail.com>
2019-11-21 18:48:44 -0500

    added wine rss and entropy plots, added code to get cluster entropies.

--- wineCluster.py
+++ wineCluster.py
@@ -3,6 +3,7 @@
 #   purpose: calculates the mean silhouette coecient and entropy
 #   of the k clusters
 
+import math
 import numpy as np
 from PIL import Image
 
@@ -11,7 +12,7 @@ from matplotlib.image import imread
 import matplotlib.pyplot as plt
 
 from sklearn.cluster import KMeans
-from sklearn.metrics import pairwise_distances_argmin_min
+from sklearn.metrics import pairwise_distances_argmin_min, cluster
 from sklearn.preprocessing import StandardScaler
 
 import argparse
@@ -25,28 +26,26 @@ def parseArguments():
                         dest='k', default="", type=int, required=True,
                         help='number of clusters')
 
-
     return parser.parse_args()
 
 
 def main():
+
     parms = parseArguments()
     k = parms.k
     print("Number of clusters: ", k)
 
     # load in the data wine set.
     wine_data = datasets.load_wine()
+    # get target labels for calculating entropy.
+    target_labels = wine_data.target
     wine_data = wine_data.data
 
     means = KMeans(init='random', n_init=1, max_iter=100, n_clusters=k, verbose='true')
     means.fit(wine_data)
+    # calculates the entropies of the clusters generated. this is not scaled however.
+    print("clusters entropy calc: ", cluster.entropy(means.labels_))
 
-    cluster_centers = means.cluster_centers_
-    # print("Cluster centers shape : ", cluster_centers.shape)
-    # print("Cluster centers print: ", cluster_centers)
-    # centroids = np.array([list(means.cluster_centers_[label]) for label in means.labels_])
-
-    # print("Centroids shape : ", centroids.shape)
 
 if __name__ == '__main__':
     main()
